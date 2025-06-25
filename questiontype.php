590c06979bf36e91885a3370903ccaebf87fa1f2
Sascha Vogel <sascha.vogel@ffhs.ch>
2025-06-25 17:07:42 +0200

    Add workaround on invalidcoursemoduleid when duplicating quizzes with coderunnner questions

--- questiontype.php
+++ questiontype.php
@@ -594,8 +594,12 @@ class qtype_coderunner extends question_type {
             $frontpageqbank = question_bank_helper::get_default_open_instance_system_type(get_site());
             $contextids[] = $frontpageqbank->context->id;
             $course = $context->get_course_context(false);
-            $courseid = $course->instanceid;
             if ($course) {
+                $courseid = $course->instanceid;
+
+                // Reset course cache (Fix for invalidcoursemoduleid on duplicating quizzes with coderunner questions).
+                rebuild_course_cache($courseid, true);
+
                 $allcaps = [];  // We don't want to restrict access to prototypes by students.
                 $sharedbanks = question_bank_helper::get_activity_instances_with_shareable_questions([$courseid], [], $allcaps);
                 $privatebanks = question_bank_helper::get_activity_instances_with_private_questions([$courseid], [], $allcaps);
